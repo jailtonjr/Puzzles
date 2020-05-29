5f6c09c14d87b9389120add7f21e4a3d6cda511f
jailton louzada <jailtonalkimin@gmail.com>
2020-05-29 10:27:50 +0200

    Adding read.md file

---
+++ read.md
@@ -0,0 +1,4 @@
+Some Puzzles solved using Java
+===
+
+* [Startup Line](https://github.com/jailtonjr/Puzzles/blob/master/src/main/java/StartupLinePuzzle.java)
--- src/main/java/StartupLinePuzzle.java
+++ src/main/java/StartupLinePuzzle.java
@@ -6,12 +6,23 @@ import java.util.Arrays;
  * WebSite: https://jailtonjr.dev
  * Description: StartupLinePuzzle
  */
+
+/*For the one thousand one hundred PLCs on each line and one thousand one hundred
+ employees at the plant that participate in this the startup. The plant manager asks
+ the first employee to go to every PLC and place it in RUN mode. Then he has the
+ second employee go to every second PLC and place it in Program mode. The third
+ goes to every third PLC and, if it is in PROGRAM mode, they place it in RUN mode,
+ and if it is in RUN mode, they place it in PROGRAM mode. The fourth employee does
+ this to every fourth PLC, and so on. After the process is completed with the one
+ thousand one hundredth employee, how many PLCs are in RUN mode?*/
+
+
 public class StartupLinePuzzle {
 
     public static void main(String[] args) {
 
-        final int MAX_EMPLOYEE = 10;
-        boolean[] PLC = new boolean[10];
+        final int MAX_EMPLOYEE = 1100;
+        boolean[] PLC = new boolean[1100];
         //first employee set all PLC as RUN mode = true
         Arrays.fill(PLC, true);
 
